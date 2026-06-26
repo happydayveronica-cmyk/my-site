39aedd6b3e4ede50791643a4e3d60d0554d361e8
happydayveronica-cmyk <happydayveronica@gmail.com>
2026-06-26 09:16:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
+const schoolList = document.getElementById("schoolList");
 
+function render(){
+
+    schoolList.innerHTML="";
+
+    schools.forEach(school=>{
+
+        let html=`
+        <div class="school">
+
+            <h1>${school.name}</h1>
+            <p>${school.subtitle}</p>
+
+            <div class="grid">
+        `;
+
+        school.sections.forEach(section=>{
+
+            html+=`
+            <div class="card">
+
+                <div class="cardHeader">
+
+                    <h2>${section.title}</h2>
+
+                    <span>${section.tag}</span>
+
+                </div>
+
+                <ul>
+            `;
+
+            section.items.forEach(item=>{
+                html+=`<li>${item}</li>`;
+            });
+
+            html+=`
+                </ul>
+
+            </div>
+            `;
+        });
+
+        html+=`
+            </div>
+
+        </div>
+        `;
+
+        schoolList.innerHTML+=html;
+
+    });
+
+}
+
+render();
